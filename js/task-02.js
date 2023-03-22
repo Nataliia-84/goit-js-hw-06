feb974e9aa3a6e8d59af8ad3a03558b79e10f169
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-03-22 16:41:24 +0100

    task 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl=document.querySelector('#ingredients')
+console.log(listEl)
+
+// ingredients.forEach((item)=>{
+// const li = document.createElement("li")
+// li.classList=('item')
+// li.textContent=item
+
+// console.log(li)
+// listEl.append(li)
+// })
+const liEl=ingredients.map((item)=>{
+  const li = document.createElement("li")
+  li.classList=('item')
+  li.textContent=item
+  
+ return li
+  
+  })
+  listEl.append(...liEl)
+  console.log(liEl)
+  console.log(listEl)
