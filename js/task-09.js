bc68ce80faf5515fc3a3560af3e9e8ed280f2235
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-03-25 17:13:55 +0100

    task 9 -done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+
+const buttonChange=document.querySelector('.change-color')
+console.log(buttonChange)
+const colorChange = document.querySelector('.color')
+console.log(colorChange)
+const bodyEl = document.querySelector('body')
+console.log(bodyEl)
+buttonChange.addEventListener('click',onClickButtonChange)
+
+function onClickButtonChange(event) {
+  console.log(event.currentTarget)
+  colorChange.textContent = getRandomHexColor()
+  console.log(colorChange.textContent)
+  bodyEl.style.backgroundColor = colorChange.textContent;
+  
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
